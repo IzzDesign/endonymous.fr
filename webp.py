383bc709e5ad6a092184cecb084e30ebe58ed883
Neamar <neamar@neamar.fr>
2022-07-21 17:49:07 +0200

    All webp

--- webp.py
+++ webp.py
@@ -5,7 +5,7 @@ import subprocess
 PATH = './assets/roman-graphique'
 folders = [f for f in listdir(PATH) if not isfile(join(PATH, f))]
 
-for folder in folders[0:1]:
+for folder in folders:
     folder_path = join(PATH, folder)
     files = [f for f in listdir(folder_path) if isfile(join(folder_path, f)) and '.png' in f]
 
